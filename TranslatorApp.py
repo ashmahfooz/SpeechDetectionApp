94a11e88c94045b8d25ee82bfd06344bdff679c7
Ashraf <ashmahfooz404@gmail.com>
2024-11-14 20:58:28 +0530

    Speech Recognition App

---
+++ TranslatorApp.py
@@ -0,0 +1,50 @@
+import os.path
+
+import speech_recognition as sr
+import streamlit as st
+import googletrans as trans
+import gtts
+import playsound
+
+st.title("Speech Recognition App")
+
+languages={'afrikaans': 'af', 'albanian': 'sq', 'amharic': 'am', 'arabic': 'ar', 'armenian': 'hy', 'azerbaijani': 'az', 'basque': 'eu', 'belarusian': 'be', 'bengali': 'bn', 'bosnian': 'bs', 'bulgarian': 'bg', 'catalan': 'ca', 'cebuano': 'ceb', 'chichewa': 'ny', 'chinese (simplified)': 'zh-cn', 'chinese (traditional)': 'zh-tw', 'corsican': 'co', 'croatian': 'hr', 'czech': 'cs', 'danish': 'da', 'dutch': 'nl', 'english': 'en', 'esperanto': 'eo', 'estonian': 'et', 'filipino': 'tl', 'finnish': 'fi', 'french': 'fr', 'frisian': 'fy', 'galician': 'gl', 'georgian': 'ka', 'german': 'de', 'greek': 'el', 'gujarati': 'gu', 'haitian creole': 'ht', 'hausa': 'ha', 'hawaiian': 'haw', 'hebrew': 'he', 'hindi': 'hi', 'hmong': 'hmn', 'hungarian': 'hu', 'icelandic': 'is', 'igbo': 'ig', 'indonesian': 'id', 'irish': 'ga', 'italian': 'it', 'japanese': 'ja', 'javanese': 'jw', 'kannada': 'kn', 'kazakh': 'kk', 'khmer': 'km', 'korean': 'ko', 'kurdish (kurmanji)': 'ku', 'kyrgyz': 'ky', 'lao': 'lo', 'latin': 'la', 'latvian': 'lv', 'lithuanian': 'lt', 'luxembourgish': 'lb', 'macedonian': 'mk', 'malagasy': 'mg', 'malay': 'ms', 'malayalam': 'ml', 'maltese': 'mt', 'maori': 'mi', 'marathi': 'mr', 'mongolian': 'mn', 'myanmar (burmese)': 'my', 'nepali': 'ne', 'norwegian': 'no', 'odia': 'or', 'pashto': 'ps', 'persian': 'fa', 'polish': 'pl', 'portuguese': 'pt', 'punjabi': 'pa', 'romanian': 'ro', 'russian': 'ru', 'samoan': 'sm', 'scots gaelic': 'gd', 'serbian': 'sr', 'sesotho': 'st', 'shona': 'sn', 'sindhi': 'sd', 'sinhala': 'si', 'slovak': 'sk', 'slovenian': 'sl', 'somali': 'so', 'spanish': 'es', 'sundanese': 'su', 'swahili': 'sw', 'swedish': 'sv', 'tajik': 'tg', 'tamil': 'ta', 'telugu': 'te', 'thai': 'th', 'turkish': 'tr', 'ukrainian': 'uk', 'urdu': 'ur', 'uyghur': 'ug', 'uzbek': 'uz', 'vietnamese': 'vi', 'welsh': 'cy', 'xhosa': 'xh', 'yiddish': 'yi', 'yoruba': 'yo', 'zulu': 'zu'}
+targeted_lang=st.selectbox("Choose language to translate to",list(languages.keys()))
+
+
+recognizer=sr.Recognizer()
+
+
+
+def capture_and_translate():
+    with st.spinner("Adjusting ambient noise......."):
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source)
+            st.write("Please speak.......")
+            audio=recognizer.listen(source)
+            st.write("Recognizing.......")
+            text=recognizer.recognize_google(audio)
+            st.success(f"You said:{text}")
+
+            translator=trans.Translator()
+            translation=translator.translate(text,dest=languages[targeted_lang])
+            st.success(f"Translation is:{translation.text}")
+
+            converted_audio=gtts.gTTS(translation.text,lang=languages[targeted_lang])
+            try:
+                audio_file="Test.mp3"
+                converted_audio.save(audio_file)
+                playsound.playsound(audio_file)
+            except Exception as e:
+                st.write(f"Error:{e}")
+
+
+            with open(audio_file,"rb") as file:
+                st.download_button("Download",file,file_name="Test.mp3")
+
+            os.remove(audio_file)
+
+
+if st.button("Start Listening"):
+    capture_and_translate()
+
